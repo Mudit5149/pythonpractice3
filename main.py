589f8a7a807f3b26e44a45097ba7605ec64a5dac
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2022-06-20 10:41:23 +0530

    Inheritence Example

---
+++ main.py
@@ -0,0 +1,23 @@
+class a:
+    def feature1(self):
+        print('feature 1 is working')
+
+    def feature2(self):
+        print('feature 2 is working')
+
+
+class b(a):
+    def feature3(self):
+        print('feature 1 is working')
+
+    def feature4(self):
+        print('feature 2 is working')
+
+
+
+a1 = a()
+a1.feature1()
+a1.feature2()
+B1 = b()
+
+
